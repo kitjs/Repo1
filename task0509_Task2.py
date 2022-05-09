52d0dc5ecd8aff57438be760de2fc53507ba3c23
yonopriienko <yonopriienko@playtika.com>
2022-05-09 16:31:49 +0200

    task add

--- task0509_Task2.py
+++ task0509_Task2.py
@@ -1,3 +1,13 @@
 
-text1 = "Good day"
-print(text1, end=" ")
+text1 = "Good day-2"
+#Goodd day-1 !2!4!7
+print("Goodd day-1 ",2,4,7,sep="!")
+
+#Good day-2
+print(text1,end=" ")
+
+#Goodd day-3  2 4 7
+print("Goodd day-3 ",2,4,7, end="\n")
+
+#Goodd day-4 !2!4!7
+print("Goodd day-4 ",2,4,7,sep="!")
